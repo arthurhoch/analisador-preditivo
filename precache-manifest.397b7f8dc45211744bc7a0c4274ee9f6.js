f41d94c9d56e05f8e2f907e0fc1b48c692bf5275
arthurhoch <arthur.j.h96@gmail.com>
2019-05-24 18:53:16 -0300

    Updates

---
+++ precache-manifest.397b7f8dc45211744bc7a0c4274ee9f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99a9afa9bd4d8c8f802f981a20b12591",
+    "url": "/analisador-preditivo/index.html"
+  },
+  {
+    "revision": "8d47a394e9f4a1e8df6b",
+    "url": "/analisador-preditivo/static/css/2.2a88c607.chunk.css"
+  },
+  {
+    "revision": "9f3ec9c07c33cf33fb6a",
+    "url": "/analisador-preditivo/static/css/main.5c62fa38.chunk.css"
+  },
+  {
+    "revision": "8d47a394e9f4a1e8df6b",
+    "url": "/analisador-preditivo/static/js/2.da33f143.chunk.js"
+  },
+  {
+    "revision": "9f3ec9c07c33cf33fb6a",
+    "url": "/analisador-preditivo/static/js/main.53c251e1.chunk.js"
+  },
+  {
+    "revision": "3203a30171aa8e9b3c61",
+    "url": "/analisador-preditivo/static/js/runtime~main.05581637.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.5e26c1f19984ecbffac2f99678b63932.js"
+  "/analisador-preditivo/precache-manifest.397b7f8dc45211744bc7a0c4274ee9f6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/analisador-preditivo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
